26f4e6b084c5e2cf9419f0aba6f91638e4d1bbd0
Efaideleon <109036920+Efai-De-Leon@users.noreply.github.com>
2024-01-06 16:13:21 -0800

    Initial Commit

---
+++ Abel-Translator-App/Abel_Translator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Abel_Translator_AppApp.swift
+//  Abel-Translator-App
+//
+//  Created by Efai De leon on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Abel_Translator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Abel-Translator-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Abel-Translator-App
+//
+//  Created by Efai De leon on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
